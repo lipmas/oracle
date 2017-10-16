909afde8e32b9b9d32f1e3b8371cce911c9de851
Sebby <lipmas93@gmail.com>
2017-10-16 00:49:03 -0400

    remove a couple lines- test ssh

--- truffle.js
+++ truffle.js
@@ -4,8 +4,6 @@ module.exports = {
 	    host: "localhost",
 	    port: 8545,
 	    network_id: "*",
-	    //default trx parameters
-	    //from: "0x2d2804cfdb2674f3184896fa8538237588699bad"
 	},
     }
 };
